12fe7ad9837cd6b0236b8c8337f6c35448f6ee80
Alex Rossell Hayes <alexander@rossellhayes.com>
2020-02-21 19:06:52 -0800

    Add use_example()

---
+++ R/use_example.R
@@ -0,0 +1,21 @@
+#' Create or edit an example .R file
+#'
+#' Create or edit an example .R file
+#'
+#' @param name File name, without extension; will create if it doesn't already
+#'     exist. If not specified, and you're currently in a .R file, will guess
+#'     name based on .R name.
+#'
+#' @seealso [usethis::use_r()] and [usethis::use_test()]
+#'
+#' @importFrom rlang %||%
+#' @export
+
+use_example <- function(name = NULL) {
+  name <- name %||% usethis:::get_active_r_file(path = "R")
+  name <- usethis:::slug(name, "R")
+  usethis:::check_file_name(name)
+  usethis::use_directory("examples")
+  usethis::edit_file(proj_path("examples", name))
+  invisible(TRUE)
+}
