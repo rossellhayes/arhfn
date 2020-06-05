18991364c75da58bdb4f36f86910b9c2f6d9a140
Alex Rossell Hayes <alexander@rossellhayes.com>
2020-06-04 17:49:49 -0700

    `ghosts()` no longer uses {cli}, now repeats to fill the screen, and now includes a red ghost

--- R/ghosts.R
+++ R/ghosts.R
@@ -1,34 +1,54 @@
 #' Print ghosts in the console
 #'
-#' @return
+#' To run on startup, add the following to your `~/.Rprofile`
+#' (see [usethis::edit_r_profile()]):
+#' ```
+#' if (interactive() && requireNamespace("arhfn", quietly = TRUE)) {
+#'   setHook("rstudio.sessionInit", function(isNewSession) {
+#'     rsthemes::use_theme_auto(dark_start = "18:00", dark_end = "6:00")
+#'   }, action = "append")
+#' }
+#' ```
+#'
+#' @return Invisibly returns the string "ghosts"
 #' @export
+#'
+#' @examples ghosts()
 
 ghosts <- function() {
-  n <- min(floor((getOption("width", "80")) / 12), 6)
+  n <- floor(getOption("width", "60") / 12)
 
-  colors <- c(
-    "\033[31m", "\033[32m", "\033[33m", "\033[34m", "\033[35m", "\033[36m"
+  colors <- rep(
+    c("\033[36m", "\033[35m", "\033[34m", "\033[33m", "\033[32m", "\033[31m"),
+    ceiling(n / 6)
   )
 
+  # lines <- list(
+  #   "    ▄▄▄     ",
+  #   "   ▀█▀██  ▄ ",
+  #   " ▀▄██████▀  ",
+  #   "    ▀█████  ",
+  #   "       ▀▀▀▀▄"
+  # )
+
   lines <- list(
-    "    OOO     ",
-    "   OOOOO    ",
-    "    O OO  O ",
-    " O OOOOOOO  ",
-    "  OOOOOOO   ",
-    "    OOOOOO  ",
-    "     OOOOO  ",
-    "       OOOO ",
-    "           O"
+    "    lll     ",
+    "   uwuww  l ",
+    " ulwwwwwwu  ",
+    "    uwwwww  ",
+    "       uuuul"
   )
 
   lines <- lapply(lines, rep, n)
   lines <- lapply(lines, function(x) paste0(colors[seq_len(n)], x))
   lines <- lapply(lines, paste, collapse = "")
   lines <- paste(unlist(lines), collapse = "\n")
-  lines <- paste0("\n", lines, "\033[39m\n\n")
+  lines <- paste0(lines, "\033[39m\n\n")
+  lines <- gsub("u", "\u2580", lines)
+  lines <- gsub("l", "\u2584", lines)
+  lines <- gsub("w", "\u2588", lines)
 
-  cat(gsub("O", cli::symbol$full_block, lines))
+  cat(lines)
 
   invisible("ghosts")
 }
