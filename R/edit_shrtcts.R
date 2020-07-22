ad396eb662f56cc305a7029e78c786b0dc0ce26d
Alex Rossell Hayes <alexander@rossellhayes.com>
2020-07-22 10:09:57 -0700

    Add edit_shrtcts()

---
+++ R/edit_shrtcts.R
@@ -0,0 +1,16 @@
+#' Edit shrtcts configuration file
+#'
+#' @return Path to the file, invisibly
+#' @seealso [shrtcts::add_rstudio_shortcuts()]
+#' @export
+
+edit_shrtcts <- function() {
+  path <- fs::path_home_r(".shrtcts.yaml")
+  usethis::edit_file(path)
+  usethis::ui_todo(
+    paste(
+      "Run", ui_code("add_rstudio_shortcuts()"), "for changes to take effect"
+    )
+  )
+  invisible(path)
+}
