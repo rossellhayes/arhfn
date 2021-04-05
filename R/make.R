4f965272966f20fb98f31cc28882bb1018bcce15
Alex Rossell Hayes <alexander@rossellhayes.com>
2021-04-05 13:45:02 -0700

    Document

--- R/make.R
+++ R/make.R
@@ -1,3 +1,10 @@
+#' Run makefile
+#'
+#' Sources the contents of '_make.R' in an RStudio job
+#'
+#' @return Invisilbe [`NULL`]
+#' @export
+
 make <- function() {
   rstudioapi::documentSave()
   if (file.exists("_make.R")) {
@@ -5,4 +12,5 @@ make <- function() {
   } else {
     abort_msg(paste("File", path("_make.R"), "not found."))
   }
+  invisible(NULL)
 }
