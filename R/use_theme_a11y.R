722d1752d86bb09393f216ff317d0bcbde3736ab
Alex Rossell Hayes <alexander@rossellhayes.com>
2022-06-17 00:03:48 -0700

    feat: add `use_theme_a11y()`

---
+++ R/use_theme_a11y.R
@@ -0,0 +1,19 @@
+#' Apply a color-blind friendly RStudio theme
+#'
+#' @param dark A logical indicating whether to use a dark theme.
+#'   If `NULL`, the default, the decision is based on whether the current active
+#'   theme is a light theme or a dark theme.
+#' @inheritParams rsthemes::use_theme_dark
+#'
+#' @export
+use_theme_a11y <- function(dark = NULL, quietly = FALSE) {
+  if (is.null(dark) && rstudioapi::isAvailable()) {
+    dark <- rstudioapi::getThemeInfo()$dark
+  }
+
+  apply_theme <- utils::getFromNamespace("apply_theme", "rsthemes")
+
+  style <- if (isTRUE(dark)) "dark" else "light"
+
+  apply_theme(paste0("a11y-", style, " {rsthemes}"), quietly, style = style)
+}
