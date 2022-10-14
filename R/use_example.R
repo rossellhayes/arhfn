1e2632bd2c646b66d08fa6c91837f0bb1c98d22b
Alex Rossell Hayes <alexander@rossellhayes.com>
2022-10-14 16:23:13 -0700

    fix(use_example): fix bug

--- R/use_example.R
+++ R/use_example.R
@@ -2,48 +2,78 @@
 #'
 #' Creates an R script in `man/examples` to store examples for a function.
 #'
-#' @param name File name, without extension.
-#'   If [`NULL`], the default, automatically generate a name based on the file
-#'   that is currently open.
+#' @param name Either a name without extension, or `NULL` to create the paired
+#'   file based on currently open file in the script editor.
+#'   If the `R/` or test file is open, `use_example()` will create/open the
+#'   corresponding example file.
 #' @param dir The directory where example scripts will be stored.
 #'   Defaults to `man/examples`.
+#' @inheritParams usethis::use_r
 #'
 #' @seealso [usethis::use_r()] and [usethis::use_test()]
 #'
 #' @export
 
-use_example <- function(name = NULL, dir = "man/examples") {
-  rlang::check_installed("fs", "to use `use_example()`.")
+use_example <- function(
+  name = NULL, dir = "man/examples", open = rlang::is_interactive()
+) {
+  # Determine file name
+  name <- name %||% fs::path_file(rstudioapi::getSourceEditorContext()$path)
+  name <- gsub("^test-", "", name)
+  name <- fs::path_ext_set(name, "R")
 
-  if (!is.null(name)) {
-    name <- paste0(name, ".R")
+  # Determine path to `R/` file
+  r_path <- fs::path("R", name)
+
+  # Determine path to example file
+  usethis::use_directory(dir)
+  example_name <- paste0("example-", name)
+  example_path <- fs::path(dir, example_name)
+
+  if (fs::file_exists(r_path) && rlang::is_installed("roxygen2")) {
+    # Check contents of roxygen tags in `R/` file
+    roxygen_tags <- roxygen2::parse_file(r_path)
+    roxygen_tags <- purrr::map(roxygen_tags, "tags")
+    roxygen_tags <- purrr::flatten(roxygen_tags)
+
+    # Check if `R/` file already has the path in its `@example` tag
+    example_path_tags <- roxygen_tags[roxygen_tags$tag == "example"]
+    example_path_tags <- purrr::map_chr(example_path_tags, "val")
+    example_tag_already_present <- any(example_path_tags == example_path)
+
+    # Check if `R/` file has examples in an `@examples` tag
+    examples_tags <- roxygen_tags[roxygen_tags$tag == "examples"]
+    preexisting_examples <- purrr::map_chr(examples_tags, "raw")
+    preexisting_examples <- gsub("^\\n+", "", preexisting_examples)
   } else {
-    rlang::check_installed(
-      "rstudioapi",
-      "to automatically detect the current file in `use_example()`."
-    )
-
-    current_path <- rstudioapi::getSourceEditorContext()$path
-    current_dir <- fs::path_file(fs::path_dir(current_path))
-
-    name <- fs::path_file(current_path)
-
-    if (current_dir %in% c("tests", "testthat")) {
-      name <- gsub("^test-", "", name)
-    }
-
-    name <- paste0("example-", name)
+    # Defaults if `R/` file's roxygen tags cannot be read
+    example_tag_already_present <- FALSE
+    preexisting_examples <- character(0)
   }
 
-  fs::dir_create(dir)
+  # Inform user to add `@example` tag with example path
+  if (!example_tag_already_present) {
+    if (length(preexisting_examples) > 0) {
+      usethis::ui_todo(paste(
+        "Replace", usethis::ui_code("@examples"),
+        "in your roxygen documentation with the following line:"
+      ))
+    } else {
+      usethis::ui_todo(
+        "Add the following line to your roxygen documentation:"
+      )
+    }
 
-  path <- fs::path(dir, name)
-  file.edit(path)
+    usethis::ui_line("#' @example {example_path}")
+  }
 
-  cli::cli_inform(
-    c("*" = "Add {.code #' @example {path}} to your roxygen documentation.")
-  )
-  invisible(path)
+  # If the example file doesn't already exist, copy examples from the `R/` file
+  if (!fs::file_exists(example_path)) {
+    writeLines(preexisting_examples, example_path)
+  }
+
+  usethis::edit_file(example_path, open = open)
+  invisible(example_path)
 }
 
 #' Create or edit a test file
